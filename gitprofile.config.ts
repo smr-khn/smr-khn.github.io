92982224a65c7807758c85f0c2be5a9f460902ca
Sameer Khan <sameerkhan@outlook.com>
2025-01-21 16:17:50 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -31,28 +31,28 @@ const CONFIG = {
     external: {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
-      // projects: [
-      //   {
-      //     title: 'Project Name',
-      //     description:
-      //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      //     imageUrl:
-      //       'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-      //     link: 'https://example.com',
-      //   },
-      //   {
-      //     title: 'Project Name',
-      //     description:
-      //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      //     imageUrl:
-      //       'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-      //     link: 'https://example.com',
-      //   },
-      // ],
+      projects: [
+        // {
+        //   title: 'Project Name',
+        //   description:
+        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+        //   imageUrl:
+        //     'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+        //   link: 'https://example.com',
+        // },
+        // {
+        //   title: 'Project Name',
+        //   description:
+        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+        //   imageUrl:
+        //     'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+        //   link: 'https://example.com',
+        // },
+      ],
     },
   },
   seo: {
-    title: 'Sameer Khan's Portfolio',
+    title: 'Sameer Khan Portfolio',
     description: 'My Portfolio',
     imageURL: '',
   },
